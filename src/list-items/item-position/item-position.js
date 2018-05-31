73c92454cf323ff2d32fa07445b5e0e3cedb01eb
Matkala Jani <Jani.Matkala@opuscapita.com>
2018-05-31 12:23:19 +0300

    change itemIds propType

--- src/list-items/item-position/item-position.js
+++ src/list-items/item-position/item-position.js
@@ -1,16 +1,21 @@
+import { List } from 'immutable';
+
 class ItemPosition {
   constructor() {
     this.data = this.getInitial();
   }
 
-  set = (list, itemId) => {
+  set = (srcList, itemId) => {
+    let list = srcList;
+    if (!List.isList(list)) list = List(srcList);
+
     const index = list.findIndex(i => i === itemId);
     if (index !== -1) {
       this.data.previous = index > 0 ? list.get(index - 1) : 0;
       this.data.next = index !== list.size - 1 ? list.get(index + 1) : 0;
       this.data.string = `${(index + 1)}/${list.size}`;
     }
-  }
+  };
 
   getNext = () => this.data.next;
 
